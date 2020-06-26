ac0b7549de23fc46fac7f5885f1bef85cc4067c5
Dan Swan <rodger.swan@gmail.com>
2020-06-26 20:26:41 +1000

    ajax function for current weather working

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,17 @@
-let city = $(".input").val();
-
 //add searches to save list
 function savedSearches() {
-    let a = $("<a>").addClass("panel-block").val(city);
+  const city = $(".input").val();
+    let a = $("<a>").addClass("panel-block");
     let span = $("<span>").addClass("panel-icon")
     let i = $("<i>").addClass("fas fa-city")
-    
-    $("#saved-searches").append(a, span, i);
+    let p = $("<p>").text(city);
+    span.append(i);
+    a.append(span);
+    a.append(p)
+    $("#saved-searches").append(a);
 }
 
-$(".panel-block").on("click", function(){
+$(document).on("click", ".panel-block", function(){
     $(".panel-block").removeClass("is-active");
     $(this).addClass("is-active");
 })
@@ -23,31 +25,60 @@ $("#city-search").keypress(function (event) {
 });
 
 $("#search-btn").on("click", function () {
-    console.log($(".input").val());
-
-    const city = $(".input").val();
-    const apiKey = "&appid=37c46c36e443323326f2545ed2229ed9";
-    const queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
-
-    $("#searchTerm").val("");  
-    
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-
-      console.log(response.name);
-      console.log(response.weather[0].icon);
-
-      let tempF = (response.main.temp - 273.15) * 1.8 + 32;
-      console.log(Math.floor(tempF));
-
-      console.log(response.main.humidity);
-
-      console.log(response.wind.speed);
-    });
-    
-    // savedSearches()
+  document.querySelector("#weather-data").removeAttribute("hidden");
+  savedSearches()
+  currentWeather();
+  forecastWeather()
+  $("#city-search").val("");
 });
+
+//this function retrieves api data and creates elements to populate the weather-data card with api data.
+function currentWeather() {
+  const city = $(".input").val();
+  const apiKey = "&appid=37c46c36e443323326f2545ed2229ed9";
+  const queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    ``
+    //Sets the city name for the weather data. Also sets the date with moment.js
+    document.querySelector("#current-city").textContent = response.name;
+    document.querySelector("#current-date").textContent = moment().format(
+      "MMMM Do YYYY"
+    );
+    //Sets the weather icon
+    document.querySelector("#current-weather-pic").src =
+      "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+    
+    //sets the alt text to match the icon
+    document.querySelector("#current-weather-pic").alt =
+      response.weather[0].description;
+
+    //Converts temp to fahrenheit and displays weather data
+    let tempF = (response.main.temp - 273.15) * 1.8 + 32;
+    document.querySelector("#current-temp").textContent =
+      "Temperature: " + Math.floor(tempF) + "°F";
+    document.querySelector("#current-humidity").textContent =
+      "Himidity: " + response.main.humidity + "%";
+    document.querySelector("#current-wind").textContent =
+      "Wind speed: " + response.wind.speed + "MPH";
+  });
+}
+
+//this function retrieves api forecast data and creates elements to populate the 5 day forecast cards with api data.
+function forecastWeather() {
+  const city = $(".input").val();
+  const apiKey = "&appid=37c46c36e443323326f2545ed2229ed9";
+  const queryURL =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+};
