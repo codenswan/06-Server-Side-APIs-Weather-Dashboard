d4962ec9cdae6e34c17591bfd90804146f4c09d4
Dan Swan <rodger.swan@gmail.com>
2020-06-29 16:57:54 +1000

    forecast elements created

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,18 @@
 //add searched cities to save list
-function savedSearches() {
+function citySearchList() {
   const city = $(".input").val();
-  let a = $("<a>").addClass("panel-block");
-  let span = $("<span>").addClass("panel-icon");
-  let i = $("<i>").addClass("fas fa-city");
-  let p = $("<p>").text(city);
+  const a = $("<a>").addClass("panel-block");
+  const span = $("<span>").addClass("panel-icon");
+  const i = $("<i>").addClass("fas fa-city");
+  const p = $("<p>").text(city);
   span.append(i);
   a.append(span);
   a.append(p);
   $("#saved-searches").append(a);
 }
 
-//this click event makes active the selected city from the save list and repopulates the weather data card with the correct data
-$(document).on("click", ".panel-block", function () {
+// this click event makes active the selected city from the save list and repopulates the weather data card with the correct data
+$(document).on("click", "#saved-searches", function () {
   $(".panel-block").removeClass("is-active");
   $(this).addClass("is-active");
   let city = $(this).text();
@@ -27,19 +27,25 @@ $("#city-search").keypress(function (event) {
   }
 });
 
-
 $("#search-btn").on("click", function () {
-  document.querySelector("#weather-data").removeAttribute("hidden");
+  $("#weather-data").removeAttr("hidden");
+  $(".container").removeAttr("hidden");
   const city = $(".input").val();
-  savedSearches(city);
+  const savedCities = []
+  savedCities.push(city)
+  localStorage.setItem("savedCities", JSON.stringify(savedCities))
+  console.log(localStorage)
   currentWeather(city);
+  citySearchList();
   forecastWeather(city);
+
   $("#city-search").val("");
 });
 
 //this function retrieves api data and creates elements to populate the weather-data card with api data.
 function currentWeather(city) {
-  // const city = $(".input").val();
+  if (!city) return;
+
   const apiKey = "&appid=37c46c36e443323326f2545ed2229ed9";
   const queryURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" + city + apiKey;
@@ -47,70 +53,81 @@ function currentWeather(city) {
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    //Sets the city name for the weather data. Also sets the date with moment.js
-    document.querySelector("#current-city").textContent = response.name;
-    document.querySelector("#current-date").textContent = moment().format(
-      "MMMM Do YYYY"
-    );
-    //Sets the weather icon
-    document.querySelector("#current-weather-pic").src =
-      "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+  })
+    .then((response) => {
+      //Sets the city name for the weather data. Also sets the date with moment.js
+      document.querySelector("#current-city").textContent = response.name;
+      document.querySelector("#current-date").textContent = moment().format(
+        "MMMM Do YYYY"
+      );
+      //Sets the weather icon
+      document.querySelector("#current-weather-pic").src =
+        "https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
 
-    //sets the alt text to match the icon
-    document.querySelector("#current-weather-pic").alt =
-      response.weather[0].description;
+      //sets the alt text to match the icon
+      document.querySelector("#current-weather-pic").alt =
+        response.weather[0].description;
 
-    //Converts temp to fahrenheit and displays weather data
-    let tempF = (response.main.temp - 273.15) * 1.8 + 32;
-    document.querySelector("#current-temp").textContent =
-      "Temperature: " + Math.floor(tempF) + "°F";
-    document.querySelector("#current-humidity").textContent =
-      "Himidity: " + response.main.humidity + "%";
-    document.querySelector("#current-wind").textContent =
-      "Wind speed: " + response.wind.speed + "MPH";
-
-    const lat = "&lat=" + response.coord.lat;
-    const long = "&lon=" + response.coord.lon;
-    const apiKey = "&appid=37c46c36e443323326f2545ed2229ed9";
-    const queryURL =
-      "http://api.openweathermap.org/data/2.5/uvi?" + apiKey + lat + long;
-    console.log(queryURL);
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-      //gets the uv index for searched city and adds to the span element
-      document.querySelector("#current-uv").textContent =
-        "UV Index: " + response.value;
-
-      //low 1-2, moderate 3-5, or high 6-7, 8-10 very high, 11+ extreme according to http://www.bom.gov.au/uv
-      if (response.value <= 2) {
-        document
-          .querySelector("#current-uv")
-          .classList.add("has-text-white has-background-success-dark");
-      }
-
-      if (response.value <= 5 && response.value >= 3) {
-        document
-          .querySelector("#current-uv")
-          .classList.add("has-text-white has-background-warning-dark");
-      }
-
-      if (response.value >= 6 && response.value <= 11) {
-        document
-          .querySelector("#current-uv")
-          .classList.add("has-text-white has-background-danger-dark");
-      }
+      //Converts temp to fahrenheit and displays weather data
+      let tempF = (response.main.temp - 273.15) * 1.8 + 32;
+      document.querySelector(
+        "#current-temp"
+      ).textContent = `Temperature: ${Math.floor(tempF)}F`;
+      document.querySelector(
+        "#current-humidity"
+      ).textContent = `Humidity: ${response.main.humidity}%`;
+      document.querySelector(
+        "#current-wind"
+      ).textContent = `Wind speed: ${response.wind.speed}MPH`;
+      uvIndex(response);
+    })
+    .catch((err) => {
+      console.log(err);
     });
+}
+
+//the lat and lon info from the ajax response in the previous function is pass into uvIndex to get the correct city's data
+function uvIndex(response) {
+  const apiKey = "&appid=37c46c36e443323326f2545ed2229ed9";
+  const queryURL = `http://api.openweathermap.org/data/2.5/uvi?${apiKey}&lat=${response.coord.lat}&lon=${response.coord.lon}`;
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then((response) => {
+    //gets the uv index for searched city and adds to the span element
+    document.querySelector(
+      "#current-uv"
+    ).textContent = `UV Index: ${response.value}`;
+
+    //low 1-2, moderate 3-5, or high 6-7, 8-10 very high, 11+ extreme according to http://www.bom.gov.au/uv
+    let uv = document.querySelector("#current-uv");
+
+    if (response.value <= 2.0) {
+      uv.removeAttribute("class");
+      uv.classList.add("has-text-white", "has-background-success-dark");
+      return;
+    }
+
+    if (response.value <= 5.0 && response.value > 2.0) {
+      uv.removeAttribute("class");
+      uv.classList.add("has-text-white", "has-background-warning-dark");
+      return;
+    }
+
+    if (response.value > 5.0) {
+      uv.removeAttribute("class");
+      uv.classList.add("has-text-white", "has-background-danger-dark");
+      return;
+    }
   });
 }
 
 //this function retrieves api forecast data and creates elements to populate the 5 day forecast cards with api data.
 function forecastWeather(city) {
-  // const city = $(".input").val();
+  
+  //displays the weather forecast cards
+  $("#weather-forecast").empty();
   const apiKey = "&appid=37c46c36e443323326f2545ed2229ed9";
   const queryURL =
     "https://api.openweathermap.org/data/2.5/forecast?q=" + city + apiKey;
@@ -118,7 +135,47 @@ function forecastWeather(city) {
   $.ajax({
     url: queryURL,
     method: "GET",
-  }).then(function (response) {
-    console.log(response);
+  }).then((response) => {
+    let forecastResult = response.list;
+
+    for (let i = 0; i < forecastResult.length; i++) {
+      
+      if (forecastResult[i].dt_txt.indexOf("12:00:00") !== -1) {
+        // convert temp to fahrenheit
+        let temp = (forecastResult[i].main.temp - 273.15) * 1.8 + 32;
+        let tempF = Math.floor(temp);
+
+        const dayCol = $("<div>").addClass(
+          "column mt-6 mx-3 has-background-info"
+        );
+        const date = $("<p>").text(moment().format("MMMM Do YYYY"));
+
+        const icon = $("<img>").attr(
+          "src",
+          "https://openweathermap.org/img/w/" +
+            forecastResult[i].weather[0].icon +
+            ".png"
+        );
+
+        const temperature = $("<p>").text(`Temperature: ${Math.floor(tempF)}F`);
+        const humidity = $("<p>").text(
+          `Humidity: ${forecastResult[i].main.humidity}%`
+        );
+
+        dayCol.append(date, icon, temperature, humidity);
+        $("#weather-forecast").append(dayCol);
+      }
+    }
   });
 }
+
+// function init() {
+//   if (!localStorage) {
+//     return;
+//   } else {
+//     localStorage.getItem("savedCities");
+//     console.log(savedCities)
+//   }
+// }
+
+// window.onload = init()
